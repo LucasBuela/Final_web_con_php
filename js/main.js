fb29a24aa8b0fff1038ba628b278fb3bc3b5bbc4
Luka <lucas.repositoryGT@gmail.com>
2022-01-07 19:40:50 -0300

    Actualizaciones

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 ScrollReveal().reveal('.container-fluid');
 ScrollReveal().reveal('.scroll_rev',{delay:500});
+
+//afecta a las ventanas modales:
+// ScrollReveal().reveal('.scroll_left',{
+//     duration:1200,
+//     origin:"left",
+//     distance:"400px",
+// });
