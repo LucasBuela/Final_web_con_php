e2d8bba707ca97d6bc6c49242905389a87992d91
Luka <lucas.repositoryGT@gmail.com>
2022-02-08 00:49:04 -0300

    Proyecto web actualizado

--- js/main.js
+++ js/main.js
@@ -7,3 +7,33 @@ ScrollReveal().reveal('.scroll_rev',{delay:500});
 //     origin:"left",
 //     distance:"400px",
 // });
+
+//scroll-up:
+document.getElementById("button-up").addEventListener("click",scrollUp);
+
+function scrollUp(){
+
+    var currentScroll=document.documentElement.scrollTop || document.body.scrollTop;
+    
+    if(currentScroll>0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0,currentScroll-(currentScroll/10));
+        buttonUp.style.transform="scale(0)";
+
+    }
+}
+
+//Show button:
+
+buttonUp=document.getElementById("button-up");
+
+window.onscroll=function(){
+
+    var scroll=document.documentElement.scrollTop;
+    if(scroll>500){
+        buttonUp.style.transform="scale(1)";
+    }else if(scroll<500){
+        buttonUp.style.transform="scale(0)";
+    }
+
+}
